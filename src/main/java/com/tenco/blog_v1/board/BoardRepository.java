9703db701b98a8e97fca8d857fe4578977bc8e0d
CYJ <wjdtpdbs1887@naver.com>
2024-10-08 17:53:43 +0900

    게시글

--- src/main/java/com/tenco/blog_v1/board/BoardRepository.java
+++ src/main/java/com/tenco/blog_v1/board/BoardRepository.java
@@ -1,22 +1,23 @@
 package com.tenco.blog_v1.board;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
 import jakarta.persistence.TypedQuery;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @RequiredArgsConstructor
-@Repository
-public class BoardRepository {
+@Repository // IoC
+public class BoardRepository  {
 
     private final EntityManager em;
 
     /**
      * 게시글 조회 메서드
-     * @param id 조회할 게시글 id
+     * @param id 조회할 게시글 ID
      * @return 조회된 Board 엔티티, 존재하지 않으면 null 반환
      */
     public Board findById(int id) {
@@ -24,14 +25,14 @@ public class BoardRepository {
     }
 
     /**
-     * jpql의 fetch 조인 사용 - 성능 최적화
-     * 한방에 쿼리를 사용해서 즉, 직접 조인해서 데이터를 가져온다.
+     * JPQL의 FETCH 조인 사용 - 성능 최적화
+     * 한방에 쿼리를 사용해서 즉, 직접 조인해서 데이터를 가져 옵니다.
      * @param id
      * @return
      */
     public Board findByIdJoinUser(int id) {
-        // JPQL -> Fetch join을 사용해 보자.
-        String jpql = "select b from Board b join fetch b.user where b.id = :id";
+        // JPQL -> Fetch join 을 사용해 보자.
+        String jpql = " SELECT b FROM Board b JOIN FETCH b.user WHERE b.id = :id ";
         return em.createQuery(jpql, Board.class)
                 .setParameter("id", id)
                 .getSingleResult();
@@ -42,15 +43,38 @@ public class BoardRepository {
      * @return 게시글 리스트
      */
     public List<Board> findAll() {
-        TypedQuery<Board> jpql = em.createQuery("select b from Board b order by id desc ", Board.class);
+        TypedQuery<Board> jpql = em.createQuery(" SELECT b FROM Board b ORDER BY b.id DESC ", Board.class);
         return jpql.getResultList();
     }
 
-    // em.persist(board); -> 비영속 상태인 엔티티를 영속상태로 전환
+    // em.persist(board) -> 비영속 상태인 엔티티를 영속상태로 전환
     @Transactional
     public Board save(Board board) {
         em.persist(board);
         return board;
     }
 
+    /**
+     * 게시글 삭제하기
+     * @param id
+     */
+    // DELETE JPA API 메서드를 활용(영속성 컨텍트), JPQL --> QDSL .. namedQuery. ...
+    @Transactional // 트랜잭션 내에서 실행되도록 보장
+    public void deleteById(int id) {
+        Query jpql =  em.createQuery("DELETE FROM Board b WHERE b.id = :id");
+        jpql.setParameter("id", id);
+        jpql.executeUpdate();
+    }
+
+    /**
+     *
+     *  JPA API 만들어 보세요
+     */
+    // DELETE JPA API 메서드를 활용(영속성 컨텍트), JPQL --> QDSL .. namedQuery. ...
+    @Transactional // 트랜잭션 내에서 실행되도록 보장
+    public void deleteByIdJPA(int id) {
+        Board board = em.find(Board.class, id);
+        em.remove(board);
+    }
 }
+
